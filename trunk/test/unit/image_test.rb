341f36596a9ab7a128fc2d269b8d695ce237182d
irenehaque15 <irenehaque2010@u.northwestern.edu>
2011-05-16 23:31:53 -0500

    image test file

--- trunk/test/unit/image_test.rb
+++ trunk/test/unit/image_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class ImageTest < ActiveSupport::TestCase
   test "should only upload an image with a unique name" do
       image1 = Image.create(:name => 'FileName')
-      assert !Image.create(:name => 'FileName'), "Uploaded images with duplicate names"
+      image2 = Image.new
+      image2.name = 'FileName'
+      assert !image2.save, "Uploaded images with duplicate names"
   end
 end
